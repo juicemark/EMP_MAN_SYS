4cd7820558cd20a92646ae11c7a15e13a6a5cb25
juicemark <145185339+juicemark@users.noreply.github.com>
2023-10-28 04:26:17 +0800

    Add files via upload

---
+++ emp_app/urls.py
@@ -0,0 +1,49 @@
+"""
+URL configuration for Emp_Sys_Man project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.shortcuts import redirect
+
+
+urlpatterns = ([
+    path('', views.index, name='index'),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path('index', views.index, name='index'),
+    path('all_emp', views.all_emp, name='all_emp'),
+    path('add_emp', views.add_emp, name='add_emp'),
+    path('add_emp/', views.add_emp, name='add_emp'),
+    path('remove_emp', views.remove_emp, name='remove_emp'),
+    path('remove_emp/', views.remove_emp, name='remove_emp'),
+    path('remove_emp<int:emp_id>', views.remove_emp, name='remove_emp'),
+    path('filter_emp', views.filter_emp, name='filter_emp'),
+    path('memo_form', views.memo_create, name='memo_form'),
+    path('memo_list', views.memo_list, name='memo_list'),
+    path('', views.memo_list, name='memo_list'),
+    path('create/', views.memo_create, name='memo_create'),
+    path('update/<int:pk>/', views.memo_update, name='memo_update'),
+    path('delete/<int:pk>/', views.memo_delete, name='memo_delete'),
+    path('index_mail', views.index_mail, name='index_mail'),
+    path('create_mail', views.Mails, name='create_mail'),
+    path('sent_mail', views.sent_mail, name='sent_mail'),
+    path('received_mail', views.received_mail, name='received_mail'),
+    path('reply_mail', views.Reply, name="reply_mail"),
+
+     ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) +
+               static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT))
---
+++ emp_app/views.py
@@ -0,0 +1,167 @@
+from django.shortcuts import render, HttpResponse
+from .models import Employee, Role, Department
+from datetime import datetime
+from django.db.models import Q
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from .models import Memo
+from emp_app.forms import MemoForm
+from .models import Mail
+
+# Create your views here.
+@login_required
+def index(request):
+    return render(request, 'index.html')
+
+@login_required
+def all_emp(request):
+
+    emps = Employee.objects.all()
+    context = {
+        'emps': emps
+    }
+    print(context)
+    return render(request, 'view_all_emp.html', context)
+
+@login_required
+def add_emp(request):
+    if request.method == 'POST':
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        salary = int(request.POST['salary'])
+        bonus = int(request.POST['bonus'])
+        phone = int(request.POST['phone'])
+        dept = int(request.POST['dept'])
+        role = int(request.POST['role'])
+        new_emp = Employee(first_name=first_name, last_name=last_name, salary=salary, bonus=bonus, phone=phone, dept_id= dept, role_id=role, hire_date= datetime.now())
+        new_emp.save()
+        return render(request, 'add_emp.html', {'employee_added': True})
+    elif request.method == 'GET':
+        return render(request, 'add_emp.html')
+    else:
+        return HttpResponse("An exception Occurred! Employee Has Not Been Added")
+
+@login_required
+def remove_emp(request, emp_id=0):
+    employee_removed = False
+    if emp_id:
+        try:
+            emp_to_be_removed = Employee.objects.get(id=emp_id)
+            emp_to_be_removed.delete()
+            employee_removed = True
+        except Employee.DoesNotExist:
+            return HttpResponse("Please Enter A Valid EMP ID")
+    emps = Employee.objects.all()
+    context = {
+        'emps': emps,
+        'employee_removed': employee_removed,
+    }
+    return render(request, 'remove_emp.html', context)
+@login_required
+def filter_emp(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        dept = request.POST['dept']
+        role = request.POST['role']
+        emps = Employee.objects.all()
+        if name:
+            emps = emps.filter(Q(first_name__icontains = name) | Q(last_name__icontains = name))
+        if dept:
+            emps = emps.filter(dept__name__icontains = dept)
+        if role:
+            emps = emps.filter(role__name__icontains = role)
+
+        context = {
+            'emps': emps
+        }
+        return render(request, 'view_all_emp.html', context)
+
+    elif request.method == 'GET':
+        return render(request, 'filter_emp.html')
+    else:
+        return HttpResponse('An Exception Occured')
+
+def memo_list(request):
+    memos = Memo.objects.all()
+    return render(request, 'memo/memo_list.html', {'memos': memos})
+
+
+def memo_create(request):
+    if request.method == 'POST':
+        form = MemoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('memo_list')
+    else:
+        form = MemoForm()
+    return render(request, 'memo/memo_form.html', {'form': form})
+
+
+def memo_update(request, pk):
+    memo = Memo.objects.get(pk=pk)
+    if request.method == 'POST':
+        form = MemoForm(request.POST, instance=memo)
+        if form.is_valid():
+            form.save()
+            return redirect('memo_list')
+    else:
+        form = MemoForm(instance=memo)
+    return render(request, 'memo/memo_form.html', {'form': form})
+
+
+def memo_delete(request, pk):
+    memo = Memo.objects.get(pk=pk)
+    memo.delete()
+    return redirect('memo_list')
+
+
+def Mails(request):
+    if request.method == 'POST':
+        recipient = request.POST['recipient']
+        subject = request.POST['subject']
+        content = request.POST['content']
+
+
+        # Create a new Mail object and save it to the database
+        mail = Mail(recipient=recipient, subject=subject, content=content)
+        mail.save()
+
+        return render(request, 'create_mail.html', {'mail_added': True})
+    elif request.method == 'GET':
+        return render(request, 'create_mail.html')
+    else:
+        return HttpResponse("An exception occurred! Mail was not created")
+
+
+def Reply(request):
+    if request.method == 'POST':
+        recipient = request.POST['recipient']
+        subject = request.POST['subject']
+        content = request.POST['content']
+
+        # Create a new Mail object and save it to the database
+        mail = Mail(recipient=recipient, subject=subject, content=content)
+        mail.save()
+
+        return render(request, 'reply_mail.html', {'mails_added': True})
+    elif request.method == 'GET':
+        return render(request, 'reply_mail.html')
+    else:
+        return HttpResponse("An exception occurred! Mail was not created")
+
+
+
+
+def index_mail(request):
+    return render(request, 'index_mail.html')
+
+def sent_mail(request):
+    mails = Mail.objects.all()  # Query the database for all sent mails
+
+    return render(request, 'sent_mail.html', {'mails': mails})
+
+def received_mail(request):
+    mails = Mail.objects.all()
+
+
+    return render(request, 'received_mail.html', {'mails' : mails})
